981735c8dbe57490491498c4afcff6a2469f50c4
Jackson Fraga <jackson.fraga86@gmail.com>
2015-05-26 22:24:02 -0300

    add BL Aluno

---
+++ TrabalhoWeb2/src/java/com/jacksonf/bl/AlunoBL.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jacksonf.bl;
+
+import com.jacksonf.domain.Aluno;
+import com.jacksonf.utils.CrudGenerico;
+
+/**
+ *
+ * @author Jackson
+ */
+public class AlunoBL implements CrudGenerico<Aluno>{
+    
+}
