4891ad965dd2a72b450a0d4aed16c76da242763f
Jackson Fraga <jackson.fraga86@gmail.com>
2015-05-26 22:13:07 -0300

    dao jpa aluno

--- TrabalhoWeb2/src/java/com/jacksonf/dao/AlunoDAO.java
+++ TrabalhoWeb2/src/java/com/jacksonf/dao/AlunoDAO.java
@@ -11,6 +11,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 /**
  *
@@ -27,22 +28,48 @@ public class AlunoDAO implements CrudGenerico<Aluno>  {
     
     @Override
     public void salvar(Aluno bean) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        em.getTransaction().begin();
+        em.merge(bean);
+        em.getTransaction().commit();
     }
 
     @Override
     public void excluir(Aluno bean) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        em.getTransaction().begin();
+        em.remove(em.find(Aluno.class, bean.getId()));
+        em.getTransaction().commit();
     }
 
     @Override
     public List<Aluno> listar(Aluno bean) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        StringBuilder sb = new StringBuilder("select a from Aluno a where 1=1");
+        
+        if(bean.getId() != 0) {
+           sb.append("and a.id = :id");
+        }
+        
+        if(bean.getNome() != null && !bean.getNome().isEmpty()) {
+            sb.append("and a.nome = :nome");
+        }
+        
+        sb.append("order by a.nome");
+        
+        Query qry = em.createQuery(sb.toString());
+        
+        if(bean.getId() != 0) {
+            qry.setParameter("id", bean.getId());
+        }
+        
+        if(bean.getNome() != null && !bean.getNome().isEmpty()) {
+            qry.setParameter("nome", bean.getNome());
+        }
+        
+        return qry.getResultList();
     }
 
     @Override
     public Aluno consultar(Aluno bean) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return em.find(Aluno.class, bean.getId()); 
     }
     
 }
